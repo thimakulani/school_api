7b87ccc0ded9ad7ced30e04a99ca78c0417fb1e9
thimakulani <thimakulani@gmail.com>
2022-10-05 05:55:37 +0200

    update

--- school_api/Controllers/SchoolController.cs
+++ school_api/Controllers/SchoolController.cs
@@ -32,7 +32,9 @@ namespace school_api.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<School>> GetSchool(int id)
         {
-            var school = await _context.School.FindAsync(id);
+            var school = await _context.School.Where(x => x.Id == x.Id)
+                .Include("Address")
+                .FirstOrDefaultAsync(); ;
 
             if (school == null)
             {
@@ -41,63 +43,24 @@ namespace school_api.Controllers
 
             return school;
         }
-
-        // PUT: api/School/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutSchool(int id, School school)
-        {
-            if (id != school.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(school).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SchoolExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
         // POST: api/School
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<School>> PostSchool(School school)
         {
-            _context.School.Add(school);
-            await _context.SaveChangesAsync();
+            if(_context.School.ToList().Count() > 0)
+            {
+                _context.School.Update(school);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                _context.School.Add(school);
+                await _context.SaveChangesAsync();
+            }
+            
 
             return CreatedAtAction("GetSchool", new { id = school.Id }, school);
-        }
-
-        // DELETE: api/School/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteSchool(int id)
-        {
-            var school = await _context.School.FindAsync(id);
-            if (school == null)
-            {
-                return NotFound();
-            }
-
-            _context.School.Remove(school);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
         }
 
         private bool SchoolExists(int id)
